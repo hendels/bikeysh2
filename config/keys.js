00106020cb41b8217e6aa2b204cc978514a53431
p.harendarz <p.harendarz@gmail.com>
2019-01-23 15:47:28 +0100

    deploy heroku settings 2

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,11 @@
 // figures out what set of credentials to return
-var prod = false;
+var prod = true;
+
 
 if (prod) {
+    module.exports = {
+        mongoURI: process.env.MONGODB
+    };
 } else {
     module.exports = require('./dev');
 }
